2571c26c415b95c2b15d392d41674d1303bb6b18
LAPTOP-BQNGAEFP\graci <graciagarpin>
2020-09-01 19:02:39 +0200

    create_js_

---
+++ practica 02/app.js
@@ -0,0 +1,12 @@
+// Objetivo 1 [Datos del hotel]
+var hotel = {
+  name: "Hawaii",
+  location: "Playa Las Rocas",
+  img:
+    "https://lh3.googleusercontent.com/proxy/2l_FR00_-Y_SOw93k3TrBf2ur3oiB_5FuJ8EBwjJ6kkBzsz6XKtsHszJjS4IGab1ZxOdGb6VbwKW3AXHEor6G1RisWvCpKE",
+};
+
+document.getElementById("name-hotel").innerHTML = "Hotel " + hotel.name;
+document.getElementById("location-hotel").innerHTML =
+  "Ubicado en " + hotel.location;
+document.getElementById("img-hotel").src = hotel.img;
