2af80c056ce29ac7703f18f40d7185be18536d28
LAPTOP-BQNGAEFP\graci <graciagarpin>
2020-09-03 21:45:41 +0200

    show score error solved

--- practica 02/app.js
+++ practica 02/app.js
@@ -2,14 +2,14 @@
 var hotel = {
   name: "Hawaii",
   location: "Playa Las Rocas",
-  img:
-    "https://lh3.googleusercontent.com/proxy/2l_FR00_-Y_SOw93k3TrBf2ur3oiB_5FuJ8EBwjJ6kkBzsz6XKtsHszJjS4IGab1ZxOdGb6VbwKW3AXHEor6G1RisWvCpKE",
+  img: 
+    "https://lh3.googleusercontent.com/proxy/V9OvNSAp_5VlKFKlmBS199FU5vIbwmBkZi_m5p2XyEojwyE4iGrPf6jHHTt4t1qlwSJKQTo7Qi-cW5x1Kc4HhzHsmIN2t_Y"
 };
 
 document.getElementById("name-hotel").innerHTML = "Hotel " + hotel.name;
 document.getElementById("location-hotel").innerHTML =
   "Ubicado en " + hotel.location;
-document.getElementById("img-hotel").src = hotel.img;
+document.getElementById("img-hotel.Hawaii").src = hotel.img;
 
 // Objetivo 2 [Puntuación del hotel]
 var rating = prompt("Puntuación: del 1 al 5");
