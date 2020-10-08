53218a2406c3eab00568d9757ad8d9121702e041
LAPTOP-BQNGAEFP\graci <graciagarpin>
2020-10-08 19:01:36 +0200

    Calculator visible on console

---
+++ practica 04/app.js
@@ -0,0 +1,32 @@
+
+function add (numberA, numberB) {
+  var result = numberA + numberB;
+  return result;
+}
+
+var addResult = add(3,2);
+console.log(addResult);
+
+function subtract (numberA, numberB) {
+  var result = numberA - numberB;
+  return result;
+}
+
+var subtractResult = subtract(3,2);
+console.log(subtractResult);
+
+function divide (numberA, numberB) {
+  var result = numberA / numberB;
+  return result;
+}
+
+var divideResult = divide(3,2);
+console.log(divideResult);
+
+function multiply (numberA, numberB) {
+  var result = numberA * numberB;
+  return result;
+}
+
+var multiplyResult = add(3,2);
+console.log(multiplyResult);
